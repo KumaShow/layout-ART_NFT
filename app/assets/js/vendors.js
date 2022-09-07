f530e4744476087567a75b182beadf4937d8e985
KumaShow <kumadth@gmail.com>
2022-09-07 22:14:49 +0800

    Update 2022-09-07T14:14:33.716Z

--- app/assets/js/vendors.js
+++
@@ -1,7 +0,0 @@
-// masonry
-$('.masonry-row').imagesLoaded().progress( function() {
-  $('.masonry-row').masonry(); // 渲染整體畫面
-});
-
-// AOS
-AOS.init();
---
+++ assets/js/all.js
@@ -0,0 +1,142 @@
+"use strict";
+
+var currentPage = window.location.pathname; // phoneMenu
+
+function phoneMenu() {
+  var btnPhoneMenu = document.querySelector("#btnPhoneMenu");
+  var phoneMenu = document.querySelector("#phoneMenu");
+  var body = document.querySelector("body");
+  var btnNavMenu = document.querySelector("#btnNavMenu");
+  var btnNavClose = document.querySelector("#btnNavClose");
+  btnPhoneMenu.addEventListener("click", function () {
+    phoneMenu.classList.toggle("d-none");
+    body.classList.toggle("bg-mask-dark");
+    btnNavMenu.classList.toggle("d-none");
+    btnNavClose.classList.toggle("d-none");
+  }); // search bar 手機板切換
+
+  var btnPhoneSearch = document.querySelector("#btnPhoneSearch");
+  var btnSearchArrow = document.querySelector("#btnSearchArrow");
+  var searchPhoneNav = document.querySelector("#searchPhoneNav");
+  btnPhoneSearch.addEventListener("click", function () {
+    searchPhoneNav.classList.remove("d-none");
+    searchPhoneNav.classList.add("animation-from-left");
+  });
+  btnSearchArrow.addEventListener("click", function () {
+    searchPhoneNav.classList.add("d-none");
+    searchPhoneNav.classList.remove("animation-from-left");
+  });
+} // 藝術品系列頁面 篩選箭頭切換
+
+
+function filterArrow() {
+  var filters = document.querySelectorAll('.filter-button');
+  filters.forEach(function (item) {
+    item.addEventListener("click", function () {
+      var btnArrows = item.querySelectorAll('i');
+      btnArrows.forEach(function (btnArrow) {
+        btnArrow.classList.toggle("d-none");
+      });
+    });
+  });
+} // 藝術品系列頁面 篩選條件按鈕
+
+
+function btnFilter() {
+  var btnFilter = document.querySelector("#btnFilter");
+  var btnFilterClose = document.querySelector("#btnFilterClose");
+  var btnFilterCheck = document.querySelector("#btnFilterCheck");
+  var filterModal = document.querySelector("#filterModal");
+  var filterBackdrop = document.querySelector("#filterBackdrop");
+  btnFilter.addEventListener("click", function () {
+    openFilter();
+  });
+  btnFilterClose.addEventListener("click", function () {
+    closeFilter();
+  });
+  filterBackdrop.addEventListener("click", function () {
+    closeFilter();
+  });
+
+  function openFilter() {
+    filterBackdrop.classList.add("filter-backdrop");
+    filterModal.classList.add("filter");
+    filterModal.classList.remove("d-none");
+    btnFilterCheck.addEventListener("click", function () {
+      closeFilter();
+    });
+  }
+
+  function closeFilter() {
+    filterModal.classList.remove("filter");
+    filterModal.classList.add("d-none");
+    filterBackdrop.classList.remove("filter-backdrop");
+  }
+} // 市場排行 + / - 切換
+
+
+function rankingBtnToggle() {
+  var rankingItem = document.querySelectorAll('.ranking-item'); // this 問題待處理
+
+  rankingItem.forEach(function (item) {
+    item.addEventListener("click", function () {
+      if (item.classList.contains("triangle-br-plus")) {
+        item.classList.toggle("triangle-br-minus");
+      }
+    });
+  });
+}
+
+phoneMenu();
+
+if (currentPage == '/artworks.html') {
+  btnFilter();
+  filterArrow();
+} else if (currentPage == '/marketRanking.html') {
+  rankingBtnToggle();
+} else if (currentPage == '/artworks-detail.html') {
+  filterArrow();
+}
+"use strict";
+
+var swiperArtist = new Swiper('.swiperArtist', {
+  loop: true,
+  slidesPerView: 1,
+  simulateTouch: true,
+  grabCursor: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  breakpoints: {
+    992: {
+      slidesPerView: 3,
+      centeredSlides: true
+    }
+  }
+});
+var swiperOther = new Swiper(".swiperOther", {
+  slidesPerView: 2,
+  spaceBetween: 24,
+  loop: true,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  breakpoints: {
+    992: {
+      slidesPerView: 4,
+      spaceBetween: 24
+    }
+  }
+});
+"use strict";
+
+// masonry
+$('.masonry-row').imagesLoaded().progress(function () {
+  $('.masonry-row').masonry(); // 渲染整體畫面
+}); // AOS
+
+AOS.init();
+//# sourceMappingURL=all.js.map
