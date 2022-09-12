40b9d5d91bcf71856e6d41660d5d04f90452242f
KumaShow <kumadth@gmail.com>
2022-09-12 20:54:32 +0800

    加入註解

--- app/assets/js/swiper.js
+++ app/assets/js/swiper.js
@@ -1,3 +1,4 @@
+// index 首頁 - Artist 熱門藝術家
 const swiperArtist = new Swiper('.swiperArtist', {
   loop: true,
   slidesPerView: 1,
@@ -15,6 +16,7 @@ const swiperArtist = new Swiper('.swiperArtist', {
   }
 });
 
+// artworks-detail 藝術品細項 - Others 其餘作品
 const swiperOther = new Swiper(".swiperOther", {
   slidesPerView: 2,
   spaceBetween: 24,
