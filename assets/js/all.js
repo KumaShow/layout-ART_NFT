6a7ce7e9fe8dc5f07e1ad67da5685f95a254fbfc
KumaShow <kumadth@gmail.com>
2022-09-12 21:44:13 +0800

    Update 2022-09-12T13:44:05.059Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -102,12 +102,12 @@ function rankingBtnToggle() {
 
 phoneMenu();
 
-if (currentPage == '/artworks.html') {
+if (currentPage.includes("artworks.html")) {
   btnFilter();
   filterArrow();
-} else if (currentPage == '/marketRanking.html') {
+} else if (currentPage.includes("marketRanking")) {
   rankingBtnToggle();
-} else if (currentPage == '/artworks-detail.html') {
+} else if (currentPage.includes("artworks-detail.html")) {
   filterArrow();
 }
 "use strict";
