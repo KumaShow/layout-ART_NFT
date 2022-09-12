f678e4691968d1c34920058e6ab5635ed2dadb6a
KumaShow <kumadth@gmail.com>
2022-09-12 20:47:46 +0800

    masonry 預設垂直顯示

--- app/assets/js/vendors.js
+++ app/assets/js/vendors.js
@@ -1,6 +1,8 @@
 // masonry
 $('.masonry-row').imagesLoaded().progress( function() {
-  $('.masonry-row').masonry(); // 渲染整體畫面
+  $('.masonry-row').masonry({
+    horizontalOrder: true
+  }); // 渲染整體畫面
 });
 
 // AOS
